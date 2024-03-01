e1165e3ac7b5337078b20abcddcc24a6b2c6c48a
amanuelgthn <amanuelgthn@gmail.com>
2024-03-01 18:44:30 +0100

    Update logo for login and sigin

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -49,13 +49,13 @@ const SignupForm = () => {
       });
       
 
-      // if (!session) {
-      //   toast({ title: "Something went wrong. Please login your new account", });
+      if (!session) {
+        toast({ title: "Something went wrong. Please login your new account", });
         
-      //   navigate("/sign-in");
+        navigate("/sign-in");
         
-      //   return;
-      // }
+        return;
+      }
 
       const isLoggedIn = await checkAuthUser();
 
@@ -76,13 +76,15 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
+        <img src="/assets/icons/DelightLogo.png" alt="logo"
+              width={200}
+              height={200} />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Create a new account
         </h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
-          To use snapgram, Please enter your details
+          To use Delight, Please enter your Credentials
         </p>
 
         <form
