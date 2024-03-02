e19ee39d4d577d3c7905a12b7dac074e09f041ab
amanuelgthn <amanuelgthn@gmail.com>
2024-03-02 17:10:33 +0100

    Working on createpost component for posting for signed in user

--- src/_root/Pages/CreatePost.tsx
+++ src/_root/Pages/CreatePost.tsx
@@ -1,10 +1,21 @@
+import PostForm from '@/components/forms/PostForm';
 import React from 'react'
 
 const CreatePost = () => {
   return (
     <div className='flex flex-1'>
-        <div>
+        <div className='common-container'>
+            <div className='max-w-5xl flex-start gap-3 justify-start'> 
+                <img 
+                    src='/assets/icons/add-post.svg'
+                    width={36}
+                    alt='add'
+                    title='Create Post'
+                />
+                <h2><strong>Create Post</strong></h2>
+            </div>
 
+            <PostForm />
         </div>
     </div>
   )
