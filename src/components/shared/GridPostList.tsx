7dd8080f5980c5c7b7b8b4b263eb130d1125ad1a
amanuelgthn <amanuelgthn@gmail.com>
2024-03-05 13:34:50 +0100

    Working on Explore.tsx

---
+++ src/components/shared/GridPostList.tsx
@@ -0,0 +1,26 @@
+import { useUserContext } from "@/context/AuthContext"
+import { Models } from "appwrite"
+import { Link } from "react-router-dom"
+
+type GridPostListProps = {
+  posts: Models.Document[]
+}
+
+
+const GridPostList = ({ posts }: GridPostListProps) => {
+  const { user } = useUserContext()
+
+  return (
+    <ul className="grid-container">
+      {posts.map((post) => (
+        <li key={post.$id} className="relative min-w-80 h-80">
+          <Link to={`/posts/${post.$id}`}>
+            <img src="{post.imageUrl" />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default GridPostList
